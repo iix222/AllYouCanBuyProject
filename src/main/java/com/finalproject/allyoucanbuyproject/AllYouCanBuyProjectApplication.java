4f825c6ab8a04cc95e130efeafc0e0db0b56fdc1
Igor <igor.grisin@gmail.com>
2023-05-08 18:45:01 +0300

    Initial commit

---
+++ src/main/java/com/finalproject/allyoucanbuyproject/AllYouCanBuyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.allyoucanbuyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllYouCanBuyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllYouCanBuyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/allyoucanbuyproject/AllYouCanBuyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.allyoucanbuyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllYouCanBuyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
