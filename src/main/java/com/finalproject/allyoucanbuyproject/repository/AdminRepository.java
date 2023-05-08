94999e051feee447562da28474a8e2be7a4a738e
Igor <igor.grisin@gmail.com>
2023-05-08 20:05:59 +0300

    added some annotations

--- src/main/java/com/finalproject/allyoucanbuyproject/repository/AdminRepository.java
+++ src/main/java/com/finalproject/allyoucanbuyproject/repository/AdminRepository.java
@@ -3,5 +3,7 @@ package com.finalproject.allyoucanbuyproject.repository;
 
 import com.finalproject.allyoucanbuyproject.model.AdminModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends JpaRepository<AdminModel, Long> {}
--- src/main/java/com/finalproject/allyoucanbuyproject/repository/UserRepository.java
+++ src/main/java/com/finalproject/allyoucanbuyproject/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.finalproject.allyoucanbuyproject.repository;
 
 import com.finalproject.allyoucanbuyproject.model.UserModel;
+import jdk.jfr.Registered;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 }
