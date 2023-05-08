b2b5c248784cc03bcea6cb97c2051ca7ccf05d77
Igor <igor.grisin@gmail.com>
2023-05-08 20:18:19 +0300

    added ProductService

--- src/main/java/com/finalproject/allyoucanbuyproject/service/ProductService.java
+++ src/main/java/com/finalproject/allyoucanbuyproject/service/ProductService.java
@@ -1,4 +1,23 @@
 package com.finalproject.allyoucanbuyproject.service;
 
+import com.finalproject.allyoucanbuyproject.model.ProductModel;
+import com.finalproject.allyoucanbuyproject.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public ProductModel save(ProductModel product) {
+        return productRepository.save(product);
+    }
+
+    public List<ProductModel> getAllProducts() { return productRepository.findAll(); }
+
 }
