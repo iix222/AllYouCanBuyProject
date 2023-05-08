c8b838d058c990065c601547dcc4ff22812950fa
Igor <igor.grisin@gmail.com>
2023-05-08 20:08:54 +0300

    added AdminService

--- src/main/java/com/finalproject/allyoucanbuyproject/service/AdminService.java
+++ src/main/java/com/finalproject/allyoucanbuyproject/service/AdminService.java
@@ -1,4 +1,14 @@
 package com.finalproject.allyoucanbuyproject.service;
 
+import com.finalproject.allyoucanbuyproject.model.AdminModel;
+import com.finalproject.allyoucanbuyproject.repository.AdminRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminService {
+    private final AdminRepository adminRepository;
+
+    public AdminService(AdminRepository adminRepository) { this.adminRepository = adminRepository; }
+
+    public void save(AdminModel adminModel) { adminRepository.save(adminModel); }
 }
