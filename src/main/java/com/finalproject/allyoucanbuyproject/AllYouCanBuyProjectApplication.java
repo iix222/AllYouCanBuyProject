409b9f912b477641f3e5bb7a7850146945e87f95
Igor <igor.grisin@gmail.com>
2023-05-22 18:18:26 +0300

    updated AllYouCanBuyProjectApplication

--- src/main/java/com/finalproject/allyoucanbuyproject/AllYouCanBuyProjectApplication.java
+++ src/main/java/com/finalproject/allyoucanbuyproject/AllYouCanBuyProjectApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 
@@ -39,11 +40,13 @@ public class AllYouCanBuyProjectApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		OrderModel orderModel = new OrderModel();
+		BigDecimal cost = new BigDecimal("50.00");
 		orderModel.setUserAddress("Riia street");
-		//orderModel.setTotalCost()
+		orderModel.setTotalCost(cost);
 		orderModel.setDeliveryAddress("Tallinn");
 		orderModel.setOrderDate(LocalDate.of(2023, 1, 23));
 		orderModel.setUserName("JaneDoe");
 		orderService.save(orderModel);
 	}
 }
+
